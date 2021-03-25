de44035ff1c4c96bf1434c3a0efb0f7a85eb4874
Victoria <victoria.ad40@gmail.com>
2021-03-25 11:47:04 -0300

    Fix ( \n is identified )

--- src/Constants/TextConstants.java
+++ src/Constants/TextConstants.java
@@ -8,7 +8,7 @@ public class TextConstants {
     public static final String DECREASE_BPM = "BPM-";
     public static final String INCREASE_VOLUME = "+";
     public static final String RESET_VOLUME = "-";
-    public static final String RANDOM_INSTRUMENT = "\n";
+    public static final String RANDOM_INSTRUMENT = "\\n";
     public static final String RANDOM_NOTE_OP1 = ".";
     public static final String RANDOM_NOTE_OP2 = "?";
     public static final String REPEAT_NOTE_OP1 = "I";
