d782beb8f639fe3b5965d1b2b2962a2d2b18a659
Victoria <victoria.ad40@gmail.com>
2021-03-28 17:45:40 -0300

    Feat ( converter refactoring )

--- src/Services/TextConvertorService.java
+++ src/Services/TextConvertorService.java
@@ -1,9 +1,8 @@
 package Services;
 
-import Constants.ConstraintsConstants;
-import Constants.JFugueMusicConstants;
 import Constants.TextConstants;
 import Music.NoteEnum;
+
 import java.util.ArrayList;
 
 public class TextConvertorService implements ITextConvertorService{
@@ -14,94 +13,112 @@ public class TextConvertorService implements ITextConvertorService{
     }
 
     public String convert(String raw_text, int initialBpm, int initialInstrument) {
-        int currentVolume = ConstraintsConstants.MAX_VOLUME;
-        int currentInstrument = initialInstrument;
+        String resultString = cleanString(raw_text);
+        ArrayList<String> manipulationArray;
 
-        ArrayList<String> resultArray = new ArrayList<>();
-        NoteEnum lastNote = null;
+        manipulationArray = setVolume(resultString.toCharArray());
+        manipulationArray = setInstruments(manipulationArray, initialInstrument);
+        manipulationArray = setNotesOnOctaves(manipulationArray);
 
-        resultArray.add(JFugueMusicConstants.INSTRUMENT + initialInstrument);
-        resultArray.add(JFugueMusicConstants.BPM + initialBpm);
+        resultString = convertArrayToString(manipulationArray);
+        resultString = "T" + initialBpm + " " + resultString;
 
-        for(int character = 0; character < raw_text.length(); character++){
-            switch (raw_text.charAt(character)) {
-                case 'A':
-                case 'B':
-                case 'C':
-                case 'D':
-                case 'E':
-                case 'F':
-                case 'G':
-                    lastNote = NoteEnum.valueOf(String.valueOf(raw_text.charAt(character)));
-                    resultArray.add(String.valueOf(lastNote.getValue() + currentOctave * 12));
-                    break;
-                case '.':
-                case '?':
-                    if(currentOctave < 9) {
-                        currentOctave ++;
-                    } else {
-                        currentOctave = 0;
-                    }
-                    break;
-                case ' ':
-                    if(currentVolume < 64) {
-                        currentVolume *= 2;
-                    } else {
-                        currentVolume = 0;
-                    }
-                    resultArray.add(":CON(7," + currentVolume + ")");
-                    break;
-                case '!':
-                    resultArray.add("I113");
-                    currentInstrument = 113;
-                    break;
-                case '\n':
-                    resultArray.add("I14");
-                    currentInstrument = 14;
-                    break;
-                case ';':
-                    resultArray.add("I75");
-                    currentInstrument = 75;
-                    break;
-                case ',':
-                    resultArray.add("I19");
-                    currentInstrument = 19;
-                    break;
-                case 'i':
-                case 'I':
-                case 'o':
-                case 'O':
-                case 'u':
-                case 'U':
-                    resultArray.add("I6");
-                    currentInstrument = 6;
-                    break;
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    resultArray.add("I" + (currentInstrument + Character.digit(raw_text.charAt(character), 10)));
-                    break;
-                default:
-                    assert lastNote != null;
-                    resultArray.add(String.valueOf(lastNote.getValue() + currentOctave * 12));
-                    break;
+        return resultString;
+    }
+
+    private String convertArrayToString(ArrayList<String> manipulationArray) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Object substring : manipulationArray) {
+            stringBuilder.append(substring).append(TextConstants.BLANK_SPACE);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String cleanString(String text){
+        text = text.replaceAll("[RT]", "r");
+        text = text.replaceAll("[IO]", "i");
+        return text;
+    }
+
+    private ArrayList<String> setVolume(char[] text) {
+        int currentVolume = 127;
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        for (char c : text) {
+            if (c == ' ') {
+                if (currentVolume < 64) {
+                    currentVolume *= 2;
+                } else {
+                    currentVolume = 40;
+                }
+                arrayList.add(":CON(7," + currentVolume + ")");
+            } else {
+                arrayList.add(String.valueOf(c));
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (String s : resultArray) {
-            sb.append(s).append(TextConstants.BLANK_SPACE);
-        }
-        String str = sb.toString();
-        System.out.println(str);
+        return arrayList;
+    }
 
-        return str;
+    private ArrayList<String> setInstruments(ArrayList<String> text, int initialInstrument) {
+        int currentInstrument = initialInstrument;
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("I" + currentInstrument);
+
+        for (String substring : text) {
+            if (substring.contains("!")) {
+                arrayList.add("I113");
+                currentInstrument = 113;
+            } else if (substring.contains("\n")) {
+                arrayList.add("I14");
+                currentInstrument = 14;
+            } else if (substring.contains(";")) {
+                arrayList.add("I75");
+                currentInstrument = 75;
+            } else if (substring.contains(",") && !substring.contains("CON")) {
+                arrayList.add("I19");
+                currentInstrument = 19;
+            } else if ("iouU".contains(substring)) {
+                arrayList.add("I6");
+                currentInstrument = 6;
+            } else if ("0123456789".contains(substring)) {
+                arrayList.add("I" + (currentInstrument + Character.digit(Integer.parseInt(substring), 10)));
+            } else {
+                arrayList.add(substring);
+            }
+        }
+
+        return arrayList;
+    }
+
+    private ArrayList<String> setNotesOnOctaves(ArrayList<String> text) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        NoteEnum lastNote = null;
+
+        for (String substring : text) {
+            if (substring.contains("CON") || substring.contains("I")) {
+                arrayList.add(substring);
+            } else if ("ABCDEFG".contains(substring)) {
+                lastNote = NoteEnum.valueOf(substring);
+                arrayList.add(String.valueOf(lastNote.getValue() + currentOctave * 12));
+            } else {
+                if ("abcdefg".contains(substring) && lastNote != null) {
+                    arrayList.add(String.valueOf(lastNote.getValue() + currentOctave * 12));
+                } else if (".?".contains(substring)) {
+                    if (currentOctave < 9) {
+                        currentOctave++;
+                    } else {
+                        currentOctave = 0;
+                    }
+                } else {
+                    arrayList.add("R");
+                }
+                lastNote = null;
+            }
+        }
+
+        return arrayList;
     }
 }
